0f0477179c18e8675f9c37328cea46d467ead1b5
zhangdenghui <ustbdenghui@126.com>
2017-01-30 23:08:22 +0800

    ignore .idea project target

--- build.sbt
+++ build.sbt
@@ -2,5 +2,15 @@ name := "TransE-Spark"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
-    
+scalaVersion := "2.10.6"
+
+libraryDependencies ++= Seq(
+  "org.apache.commons" % "commons-math3" % "3.5",
+  "org.apache.commons" % "commons-lang3" % "3.4",
+  "org.apache.spark" %% "spark-core" % "1.6.0",
+  "org.apache.spark" %% "spark-sql" % "1.6.0",
+  "org.apache.spark" %% "spark-graphx" % "1.6.0",
+  "com.github.scopt" %% "scopt" % "3.2.0",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+  "com.databricks" %% "spark-csv" % "1.5.0"
+)
--- src/main/scala/TransE.scala
+++ src/main/scala/TransE.scala
@@ -1,6 +1,43 @@
 /**
   * Created by zhangdenghui on 17/1/30.
   */
+import org.apache.spark.{SparkConf, SparkContext}
+import scala.collection.mutable.Map
 object TransE {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName(s"TransE-Spark")
+      .set("spark.hadoop.validateOutputSpecs", "false")
+      .setMaster("local[4]")
 
+    val sc = new SparkContext(conf)
+    val f1 = sc.textFile("/Users/zhangdenghui/TransE/FB15k/entity2id.txt")
+    val f2 = sc.textFile("/Users/zhangdenghui/TransE/FB15k/relation2id.txt")
+
+    var relation2id = Map[String,Int]()
+    var entity2id = Map[String,Int]()
+    var id2entity = Map[Int,String]()
+    var id2relation = Map[Int,String]()
+
+    val EntityAndId = f1.map{line=>
+      val re=line.split("\t")
+      (re(0),re(1).toInt) }.collect()
+
+    val RelationAndId = f2.map{line=>
+      val re=line.split("\t")
+      (re(0),re(1).toInt) }.collect()
+
+    EntityAndId.map{tuple => entity2id(tuple._1) = tuple._2
+      id2entity(tuple._2)=tuple._1
+    }
+    RelationAndId.map{tuple => relation2id(tuple._1) = tuple._2
+      id2relation(tuple._2)=tuple._1
+    }
+
+
+
+
+
+
+  }
 }
