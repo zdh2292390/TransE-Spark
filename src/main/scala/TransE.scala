5d64d29ee6e2c63217c6eaed12cbdcfc296cc7fb
zhangdenghui <ustbdenghui@126.com>
2017-02-02 11:06:36 +0800

    half finished

--- src/main/scala/TransE.scala
+++ src/main/scala/TransE.scala
@@ -12,31 +12,40 @@ object TransE {
   var left_entity, right_entity = Map[Int,Map[Int,Int]]()
   var fb_h, fb_r, fb_l = ArrayBuffer[Int]()
   var ok = Map[Pair[Int,Int],Map[Int,Int]]()
-  var relation_vec = ArrayBuffer[Array[Double]]()
-  var entity_vec = ArrayBuffer[Array[Double]]()
+  var relation_vec = Array[Array[Double]]()
+  var entity_vec = Array[Array[Double]]()
+  var version :String = ""
+  var entity_num, relation_num, train_num :Int = 0
+  val RAND_MAX = 0x7fffffff
+  val pi = 3.1415926535897932384626433832795
+  var random = new util.Random
+
+  var nepoch,nbatches,batchsize :Int =1
+  var res = 0.0
+  var samples = ArrayBuffer[(Int,Int,Int)]()
 
 
   def prepare(EntityAndId: Array[(String,Int)],
               RelationAndId: Array[(String,Int)],
               Traindata: Array[(String,String,String)]): Unit = {
 
-    EntityAndId.map{
+    EntityAndId.foreach{
       tuple =>
         entity2id(tuple._1) = tuple._2
         id2entity(tuple._2) = tuple._1
     }
-    RelationAndId.map{
+    RelationAndId.foreach{
       tuple =>
         relation2id(tuple._1) = tuple._2
         id2relation(tuple._2) = tuple._1
     }
-    val entity_num = EntityAndId.length
-    val relation_num = RelationAndId.length
+    entity_num = EntityAndId.length
+    relation_num = RelationAndId.length
 
     println("entity_num:"+entity_num)
     println("relation_num:"+relation_num)
 
-    Traindata.map{
+    Traindata.foreach{
       re =>
         if(left_entity.contains(relation2id(re._3))){
           if(left_entity(relation2id(re._3)).contains(entity2id(re._1)))
@@ -59,25 +68,119 @@ object TransE {
         fb_h += entity2id(re._1)
         fb_r += relation2id(re._3)
         fb_l += entity2id(re._2)
+        samples += ((fb_h.last,fb_l.last,fb_r.last))
+
         if(ok.contains((entity2id(re._1),relation2id(re._3))))
           ok((entity2id(re._1),relation2id(re._3)))(entity2id(re._2)) = 1
         else
           ok((entity2id(re._1),relation2id(re._3))) = Map[Int,Int]()
     }
-
-    println("traindata num:"+fb_l.length)
+    train_num = fb_l.length
+    println("traindata num:"+train_num)
 
   }
 
-  def train(vecLen:Int,rate:Double,margin:Double,method:Int): Unit = {
+  def train(vecLen:Int,rate:Double,margin:Double,method:Int,sc:SparkContext): Unit = {
     this.vecLen = vecLen
     this.rate = rate
     this.margin = margin
     this.method = method
 
+    entity_vec = Array.ofDim[Double](entity_num,vecLen)
+    relation_vec = Array.ofDim[Double](relation_num,vecLen)
 
+    entity_vec.foreach{
+      r =>
+        for(i <- r.indices)
+          r(i) = randn(0, 1.0/vecLen, -6/math.sqrt(vecLen), 6/math.sqrt(vecLen))
+    }
+    relation_vec.foreach{
+      r =>
+        for(i <- r.indices)
+          r(i) = randn(0, 1.0/vecLen, -6/math.sqrt(vecLen), 6/math.sqrt(vecLen))
 
+        norm(r)
+    }
+    sgd(sc)
   }
+
+  def vec_len(a: Array[Double]): Double = {
+    var res = 0.0
+    a.foreach(res+=_)
+    res = math.sqrt(res)
+    res
+  }
+  def norm(a: Array[Double]): Unit = {
+    var x = vec_len(a)
+    if(x > 1)
+      for(i <- a.indices)
+        a(i) = a(i)/x
+  }
+
+  def rand(min: Double, max: Double): Double = {
+    min + (max-min)*util.Random.nextInt()/(RAND_MAX+1.0)
+  }
+
+  def normal(x: Double, miu: Double, sigma:Double ): Double = {
+    1.0/math.sqrt(2*pi)/sigma*math.exp(-1*(x-miu)*(x-miu)/(2*sigma*sigma))
+  }
+
+  def randn(miu: Double, sigma: Double, min: Double, max: Double): Double = {
+    var x,y,dScope :Double = 0.0
+    do{
+      x = rand(min,max)
+      y = normal(x,miu,sigma)
+      dScope = rand(0.0,normal(miu,miu,sigma))
+    }while(dScope > y)
+    x
+  }
+
+  def sgd(sc:SparkContext): Unit = {
+
+    var samplesRDD = sc.parallelize(samples,5).persist()
+    for(epoch <- 0 until nepoch){
+      res = 0
+      var start = System.currentTimeMillis()
+      for(batch <- 0 until nbatches){
+        val bcRelation_vec = sc.broadcast(relation_vec)
+        val bcEntity_vec = sc.broadcast(entity_vec)
+
+        var sampledRDD = samplesRDD
+          .sample(withReplacement = false, batchsize/train_num.toDouble, System.currentTimeMillis())
+
+        sampledRDD.mapPartitions{
+          iter =>
+            var EntityVecMap = Map[Int,Array[Double]]()
+            var RelationVecMap = Map[Int,Array[Double]]()
+            iter.map{
+              p=>
+                if(!EntityVecMap.contains(p._1))
+                  EntityVecMap(p._1) = bcEntity_vec.value(p._1)
+                if(!EntityVecMap.contains(p._2))
+                  EntityVecMap(p._2) = bcEntity_vec.value(p._2)
+                if(!RelationVecMap.contains(p._3))
+                  RelationVecMap(p._3) = bcRelation_vec.value(p._3)
+
+
+            }
+
+            EntityVecMap.iterator
+
+
+        }
+
+
+
+
+      }
+
+
+      var end = System.currentTimeMillis()
+      println("epoch"+epoch+"  res:"+res)
+      println("epoch"+epoch+" cost time:"+(end-start)/1000.0)
+    }
+  }
+
 
   def main(args: Array[String]): Unit = {
 
@@ -107,9 +210,22 @@ object TransE {
       (re(0),re(1),re(2)) }.collect()
 
     prepare(EntityAndId,RelationAndId,Traindata)
-
     var end = System.currentTimeMillis()
-    println("time:"+(end-start)/1000.0)
+    println("prepare time:"+(end-start)/1000.0)
+
+    var vecLen :Int = 100
+    var method :Int = 1
+    var rate :Double = 0.001
+    var margin : Int = 1
+    random.setSeed(System.currentTimeMillis())
+
+    nepoch = 1000
+    nbatches = 100
+    batchsize = train_num / nbatches
+    start = System.currentTimeMillis()
+    train(vecLen,rate,margin,method,sc)
+    end = System.currentTimeMillis()
+    println("train time:"+(end-start)/1000.0)
 
   }
 }
