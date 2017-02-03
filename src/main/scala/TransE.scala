a49c7145c5d65422a74095249868f5793a9e63cf
zhangdenghui <ustbdenghui@126.com>
2017-02-03 13:26:37 +0800

    finished but wrong result

--- src/main/scala/TransE.scala
+++ src/main/scala/TransE.scala
@@ -1,7 +1,7 @@
 /**
   * Created by zhangdenghui on 17/1/30.
   */
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, SparkContext,Accumulator}
 import scala.collection.mutable.{ArrayBuffer, Map}
 
 object TransE {
@@ -24,6 +24,7 @@ object TransE {
   var res = 0.0
   var samples = ArrayBuffer[(Int,Int,Int)]()
 
+  var L1_flag = true
 
   def prepare(EntityAndId: Array[(String,Int)],
               RelationAndId: Array[(String,Int)],
@@ -47,33 +48,27 @@ object TransE {
 
     Traindata.foreach{
       re =>
-        if(left_entity.contains(relation2id(re._3))){
-          if(left_entity(relation2id(re._3)).contains(entity2id(re._1)))
-            left_entity(relation2id(re._3))(entity2id(re._1)) += 1
-          else
-            left_entity(relation2id(re._3))(entity2id(re._1)) = 0
-        }
-        else
+        if(!left_entity.contains(relation2id(re._3)))
           left_entity(relation2id(re._3)) = Map[Int,Int]()
+        if(!left_entity(relation2id(re._3)).contains(entity2id(re._1)))
+          left_entity(relation2id(re._3))(entity2id(re._1)) = 0
+        left_entity(relation2id(re._3))(entity2id(re._1)) += 1
 
-        if(right_entity.contains(relation2id(re._3))){
-          if(right_entity(relation2id(re._3)).contains(entity2id(re._2)))
-            right_entity(relation2id(re._3))(entity2id(re._2)) += 1
-          else
-            right_entity(relation2id(re._3))(entity2id(re._2)) = 0
-        }
-        else
+        if(!right_entity.contains(relation2id(re._3)))
           right_entity(relation2id(re._3)) = Map[Int,Int]()
+        if(!right_entity(relation2id(re._3)).contains(entity2id(re._2)))
+          right_entity(relation2id(re._3))(entity2id(re._2)) = 0
+        right_entity(relation2id(re._3))(entity2id(re._2)) += 1
 
         fb_h += entity2id(re._1)
         fb_r += relation2id(re._3)
         fb_l += entity2id(re._2)
         samples += ((fb_h.last,fb_l.last,fb_r.last))
 
-        if(ok.contains((entity2id(re._1),relation2id(re._3))))
-          ok((entity2id(re._1),relation2id(re._3)))(entity2id(re._2)) = 1
-        else
+        if(!ok.contains((entity2id(re._1),relation2id(re._3))))
           ok((entity2id(re._1),relation2id(re._3))) = Map[Int,Int]()
+        ok((entity2id(re._1),relation2id(re._3)))(entity2id(re._2)) = 1
+
     }
     train_num = fb_l.length
     println("traindata num:"+train_num)
@@ -111,7 +106,7 @@ object TransE {
     res
   }
   def norm(a: Array[Double]): Unit = {
-    var x = vec_len(a)
+    val x = vec_len(a)
     if(x > 1)
       for(i <- a.indices)
         a(i) = a(i)/x
@@ -135,11 +130,72 @@ object TransE {
     x
   }
 
+  def randMax(x:Int): Int = {
+    var res = (random.nextInt()*random.nextInt())%x
+    while(res<0)
+      res += x
+    res
+  }
+
+  def gradient(h1:Int,l1:Int,r1:Int,h2:Int,l2:Int,r2:Int,
+               entityMap:Map[Int,Array[Double]],
+               relationMap:Map[Int,Array[Double]]): Unit = {
+    for(i <- 0 until vecLen){
+      var delta = 2*(entityMap(l1)(i)-relationMap(r1)(i)-entityMap(h1)(i))
+      if(L1_flag)
+        if(delta > 0)
+          delta = 1
+        else
+          delta = -1
+      relationMap(r1)(i) += rate*delta
+      entityMap(h1)(i) += rate*delta
+      entityMap(l1)(i) -= rate*delta
+
+      delta = 2*(entityMap(l2)(i)-relationMap(r2)(i)-entityMap(h2)(i))
+      if(L1_flag)
+        if(delta > 0)
+          delta = 1
+        else
+          delta = -1
+      relationMap(r2)(i) -= rate*delta
+      entityMap(h2)(i) -= rate*delta
+      entityMap(l2)(i) += rate*delta
+    }
+
+  }
+
+  def train_kb(h1:Int,l1:Int,r1:Int,h2:Int,l2:Int,r2:Int,
+               entityMap:Map[Int,Array[Double]],
+               relationMap:Map[Int,Array[Double]],
+               ac_res:Accumulator[Double]): Unit = {
+    val sum1 = calc_distance(h1,l1,r1,entityMap,relationMap)
+    val sum2 = calc_distance(h2,l2,r2,entityMap,relationMap)
+    if(sum1 + margin > sum2){
+      ac_res += margin + sum1 - sum2
+      gradient(h1,l1,r1,h2,l2,r2,entityMap,relationMap)
+    }
+  }
+
+  def calc_distance(h:Int, l:Int, r:Int,
+                    entityMap:Map[Int,Array[Double]],
+                    relationMap:Map[Int,Array[Double]]): Double = {
+    var sum = 0.0
+    if(L1_flag)
+      for(i <- 0 until vecLen)
+        sum += math.abs(entityMap(l)(i)-relationMap(r)(i)-entityMap(h)(i))
+    else
+      for(i <- 0 until vecLen)
+        sum += math.pow(entityMap(l)(i)-relationMap(r)(i)-entityMap(h)(i),2)
+    sum
+  }
+
   def sgd(sc:SparkContext): Unit = {
 
-    var samplesRDD = sc.parallelize(samples,5).persist()
+    val samplesRDD = sc.parallelize(samples,4).persist()
+    val bcOK = sc.broadcast(ok)
     for(epoch <- 0 until nepoch){
       res = 0
+      val ac_res = sc.accumulator[Double](0.0)
       var start = System.currentTimeMillis()
       for(batch <- 0 until nbatches){
         val bcRelation_vec = sc.broadcast(relation_vec)
@@ -148,11 +204,13 @@ object TransE {
         var sampledRDD = samplesRDD
           .sample(withReplacement = false, batchsize/train_num.toDouble, System.currentTimeMillis())
 
-        sampledRDD.mapPartitions{
+        val vecRDD = sampledRDD.mapPartitions{
           iter =>
             var EntityVecMap = Map[Int,Array[Double]]()
             var RelationVecMap = Map[Int,Array[Double]]()
-            iter.map{
+            var VecMap = ArrayBuffer[(Map[Int,Array[Double]],Map[Int,Array[Double]])]()
+
+            iter.foreach{
               p=>
                 if(!EntityVecMap.contains(p._1))
                   EntityVecMap(p._1) = bcEntity_vec.value(p._1)
@@ -161,22 +219,46 @@ object TransE {
                 if(!RelationVecMap.contains(p._3))
                   RelationVecMap(p._3) = bcRelation_vec.value(p._3)
 
+                var j = randMax(entity_num)
 
+                if(random.nextInt()%1000 < 500){
+                  while(if(bcOK.value.contains((p._1,p._3)))bcOK.value((p._1,p._3)).contains(j)else false)
+                    j = randMax(entity_num)
+                  if(!EntityVecMap.contains(j))
+                    EntityVecMap(j) = bcEntity_vec.value(j)
+                  train_kb(p._1,p._2,p._3,p._1,j,p._3,EntityVecMap,RelationVecMap,ac_res)
+                }
+                else{
+                  while(if(bcOK.value.contains((j,p._3)))bcOK.value((j,p._3)).contains(p._2)else false)
+                    j = randMax(entity_num)
+                  if(!EntityVecMap.contains(j))
+                    EntityVecMap(j) = bcEntity_vec.value(j)
+                  train_kb(p._1,p._2,p._3,j,p._2,p._3,EntityVecMap,RelationVecMap,ac_res)
+                }
+                norm(RelationVecMap(p._3))
+                norm(EntityVecMap(p._1))
+                norm(EntityVecMap(p._2))
+                norm(EntityVecMap(j))
             }
-
-            EntityVecMap.iterator
-
-
+            VecMap += ((EntityVecMap,RelationVecMap))
+            VecMap.iterator
         }
-
-
-
-
+        val vecMap = vecRDD.collect()
+        vecMap.foreach{
+          p=>
+            p._1.foreach{
+              kv=>
+                entity_vec(kv._1) = kv._2
+            }
+            p._2.foreach{
+              kv=>
+                relation_vec(kv._1) = kv._2
+            }
+        }
       }
 
-
       var end = System.currentTimeMillis()
-      println("epoch"+epoch+"  res:"+res)
+      println("epoch"+epoch+"  res:"+ac_res.value)
       println("epoch"+epoch+" cost time:"+(end-start)/1000.0)
     }
   }
